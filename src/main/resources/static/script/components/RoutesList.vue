3649070d9680fe4624310cd3fdea666a1a1c002d
wmrk <andrew.wamark@gmail.com>
2022-05-26 21:33:37 +0300

    yarn err 2

---
+++ src/main/resources/static/script/components/RoutesList.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-card  class="overflow-y-auto">
+    <v-btn>Add</v-btn>
+  <v-list>
+    <v-list-item-group
+        mandatory
+        v-model="model"
+        active-class="border"
+        color="indigo"
+    >
+      <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+      >
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      items: [
+        {
+          icon: 'mdi-wifi',
+          text: 'Wifi',
+        },
+        {
+          icon: 'mdi-bluetooth',
+          text: 'Bluetooth',
+        },
+        {
+          icon: 'mdi-chart-donut',
+          text: 'Data Usage',
+        },
+        {
+          icon: 'mdi-wifi',
+          text: 'Wifi',
+        },
+        {
+          icon: 'mdi-bluetooth',
+          text: 'Bluetooth',
+        },
+        {
+          icon: 'mdi-chart-donut',
+          text: 'Data Usage',
+        },
+        {
+          icon: 'mdi-wifi',
+          text: 'Wifi',
+        },
+        {
+          icon: 'mdi-bluetooth',
+          text: 'Bluetooth',
+        },
+        {
+          icon: 'mdi-chart-donut',
+          text: 'Data Usage',
+        },
+        {
+          icon: 'mdi-wifi',
+          text: 'Wifi',
+        },
+        {
+          icon: 'mdi-bluetooth',
+          text: 'Bluetooth',
+        },
+        {
+          icon: 'mdi-chart-donut',
+          text: 'Data Usage',
+        },
+        {
+          icon: 'mdi-wifi',
+          text: 'Wifi',
+        },
+        {
+          icon: 'mdi-bluetooth',
+          text: 'Bluetooth',
+        },
+        {
+          icon: 'mdi-chart-donut',
+          text: 'Data Usage',
+        },
+
+      ],
+      model: 1,
+    }
+  }
+
+  
+}
+</script>
+
+<style scoped>
+  .border {
+    border-right: 3px solid dodgerblue;
+  }
+</style>
