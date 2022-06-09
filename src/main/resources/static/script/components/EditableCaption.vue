e0e485e59e649e3fce820d75ca43a72831c9dbbb
wmrk <andrew.wamark@gmail.com>
2022-06-09 15:21:27 +0300

    new UI components

---
+++ src/main/resources/static/script/components/EditableCaption.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="editable-text-area-root" contenteditable @blur="onEdit" @keydown.enter="endEdit" >{{text}}</div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+  methods:{
+    onEdit(evt){
+      // var src = evt.target.innerHTML;
+      // this.txt = src;
+    },
+    endEdit(){
+      this.$el.querySelector('#editable-text-area-root').blur();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #editable-text-area-root {
+    min-width: 150px
+  }
+</style>
---
+++ src/main/resources/static/script/components/GeoMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe id="map-iframe" class="map-iframe" src="http://localhost:1234/"></iframe>
+</template>
+
+<script>
+export default {
+  props: ['track'],
+  watch: {
+    track(newTrack, oldTrack) {
+      document.getElementById("map-iframe").contentWindow.postMessage('data-url', `http://localhost:8080/api/track/${newTrack.id}/geoJson`)
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
